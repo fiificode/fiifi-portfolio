2163d5de1c8a362534e90c731ffc5f3c7c009709
Franklin Mensah <fiificode@gmail.com>
2023-11-19 22:47:34 +0000

    added hook for scroll progress

---
+++ hooks/useScrollProgress.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+const useScrollProgress = () => {
+  const [completion, setCompletion] = useState<number>(0);
+
+  useEffect(() => {
+    const updateScrollCompletion = () => {
+      const currentProgress: number = window.scrollY;
+      const scrollHeight: number =
+        document.body.scrollHeight - window.innerHeight;
+
+      if (scrollHeight) {
+        const progressRatio: number =
+          Number(currentProgress) / Number(scrollHeight);
+        const completion: number = progressRatio * 100;
+        setCompletion(Number(completion.toFixed(2)));
+      }
+    };
+
+    //event
+    window.addEventListener("scroll", updateScrollCompletion);
+    //clear event
+    return () => window.removeEventListener("scroll", updateScrollCompletion);
+  }, []);
+
+  return completion;
+};
+
+export default useScrollProgress;
