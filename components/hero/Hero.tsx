94048e7d4c5cde09fed236341d9b5a1ca45a7631
Franklin Mensah <fiificode@gmail.com>
2023-11-21 17:52:37 +0000

    hero section completed

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -93,7 +93,7 @@ const Hero = () => {
           {/**Icons */}
         </div>
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
-          <RiArrowDownSLine className="text=3xl text-basic" />
+          <RiArrowDownSLine className="text-3xl text-basic" />
         </div>
       </div>
     </section>
